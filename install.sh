28c25ac1c5a4b0282680faf966780b900eac36b0
Igor-creato <71209783+Igor-creato@users.noreply.github.com>
2025-08-25 17:44:43 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 set -euo pipefail
 
 SSH_SCRIPT_URL="https://raw.githubusercontent.com/Igor-creato/ubuntu-vps/main/ssh-setup.sh"
+CHAT_ID_URL="https://raw.githubusercontent.com/Igor-creato/ubuntu-vps/main/chat-id.sh"
 AUTO_UPDATE_URL="https://raw.githubusercontent.com/Igor-creato/ubuntu-vps/main/auto_udate_ubuntu.sh"
 DOCKER_SCRIPT_URL="https://raw.githubusercontent.com/Igor-creato/docker-install/main/install-docker.sh"
 
@@ -12,8 +13,9 @@ INSTALL_DOCKER=false
 
 for arg in "$@"; do
     case "$arg" in
-        --ssh)    INSTALL_SSH=true ;;
-        --update) AUTO_UPDATE=TRUE ;;
+        --ssh) INSTALL_SSH=true ;;
+        --chat) CHAT_ID=true ;;
+        --update) AUTO_UPDATE=true ;;
         --docker) INSTALL_DOCKER=true ;;
         *) echo "Неизвестный флаг: $arg"; exit 1 ;;
     esac
@@ -22,6 +24,7 @@ done
 # если флагов нет — ставим всё
 if [[ "$#" -eq 0 ]]; then
     INSTALL_SSH=true
+    CHAT_ID=true
     AUTO_UPDATE=true
     INSTALL_DOCKER=true
 fi
@@ -30,6 +33,11 @@ fi
 if [[ "$INSTALL_SSH" == true ]]; then
     echo "=== Установка SSH ==="
     bash <(curl -fsSL "$SSH_SCRIPT_URL")
+fi
+
+if [[ "$CHAT_ID" == true ]]; then
+    echo "=== Ваш chatid ==="
+    bash <(curl -fsSL "$CHAT_ID_URL")
 fi
 
 if [[ "$AUTO_UPDATE" == true ]]; then
